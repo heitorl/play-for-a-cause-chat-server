e76b8ed6fbf450b1e455a312988752ec12b37632
heitor lourenco <juniorio01@hotmail.com>
2023-11-30 02:54:08 -0300

    feat: implements user login and generate token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { User } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+export interface IUserPayload {
+  id: string;
+  email: string;
+  name: string;
+  iat?: number;
+  exp?: number;
+}
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(user: User) {
+    const payload: IUserPayload = {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    };
+
+    const token = this.jwtService.sign(payload);
+
+    return {
+      access_token: token,
+    };
+  }
 
   async validateUser(email: string, password: string) {
     const user: User = await this.userService.findByEmail(email);

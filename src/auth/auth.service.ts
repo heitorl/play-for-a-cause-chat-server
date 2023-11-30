b7691334a0e4e15e7f9f9de6f50f46d67fff0c35
heitor lourenco <juniorio01@hotmail.com>
2023-11-30 02:18:51 -0300

    feat: implements local auth guard and strategy

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user: User = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    throw new Error('Email address or password provided is incorrect.');
+  }
+}

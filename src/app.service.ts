126d7c0b98decf5d153519e5c436d7377de853cd
heitor lourenco <juniorio01@hotmail.com>
2023-12-04 12:44:18 -0300

    feat: create route find all message in redis

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { RedisService } from 'nestjs-redis';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly redisService: RedisService) {}
+  async getMessages(): Promise<string[]> {
+    const messages = await this.redisService
+      .getClient()
+      .lrange('sendMessage', 0, -1);
+
+    return messages.map((message) => JSON.parse(message));
   }
 }
